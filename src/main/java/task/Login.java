8d44f54a26ba0664927ab45ba45fc8a5180b6acb
SEBASTI�N OSSA QUINTERO <sebastianossa245469@correo.itm.edu.co>
2020-09-05 11:11:39 -0500

    AgregandoLogin

--- src/main/java/task/Login.java
+++ src/main/java/task/Login.java
@@ -3,9 +3,10 @@ package task;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
-import static userinterfaces.HomePage.INPUT_ClICK_SING_IN;
+import static userinterfaces.HomePage.*;
 
 public class Login implements Task {
 
@@ -13,8 +14,9 @@ public class Login implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Click.on(INPUT_ClICK_SING_IN)
-
+                Click.on(INPUT_ClICK_SING_IN),
+                Enter.theValue("sebastianossa159@gmail.com").into(INPUT_EMAIL),
+                Enter.theValue("animal123").into(INPUT_PASSWORD)
         );
     }
 
--- src/main/java/userinterfaces/HomePage.java
+++ src/main/java/userinterfaces/HomePage.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 @DefaultUrl("http://automationpractice.com/index.php")
 public class HomePage extends PageObject {
 
-    public static  final Target INPUT_ClICK_SING_IN = Target.the("Input donde click en boton login").located(By.className("login"));
+    public static final Target INPUT_ClICK_SING_IN = Target.the("Input donde click en boton login").located(By.className("login"));
+    public static final Target INPUT_EMAIL = Target.the("Input de email").located(By.id("email"));
+    public static final Target INPUT_PASSWORD =Target.the("Input the password").located(By.id("passwd"));
 
 }
